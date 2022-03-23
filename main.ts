356246f1af29d9f9176a44b775a8fe31573de97b
Smort <hello@smort.io>
2022-03-22 23:16:48 -0700

    FEAT | Obsidian plugin for Smort.io

--- main.ts
+++ main.ts
@@ -1,137 +1,120 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	addIcon,
+	App,
+	ButtonComponent,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	TextComponent,
+} from "obsidian";
+// @ts-ignore
+import smortLogo from "./SmortLogo.svg";
+// @ts-ignore
+import electron from "electron";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class Smort extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "smort-modal-command",
+			name: "Get Smort.io article as Markdown",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
+				new URLModal(this.app, this).open();
+			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+	async processURL(url: string) {
+		const activeView = this.getActiveView();
+		if (!activeView) {
+			console.error("[Smort] No active view to insert into.");
+			return;
+		}
+		const urlObj = new URL(url);
+		const articleId = urlObj.pathname.slice(1);
+		const apiURL = `https://smort.io/api/article?uuid=${articleId}&markup=markdown`;
+		// https://smort.io/f99f6422-8211-486d-b59c-1ce02f040d49
+		var body = "";
+		const request = electron.remote.net.request({
+			url: apiURL,
+		});
+		request.setHeader("Accept", "application/json");
+		request.on("response", (response: any) => {
+			response.on("end", () => {
+				if (body && body.length > 0) {
+					this.addMarkdown(body);
+				} else {
+					console.error(
+						`[Smort] Unable to fetch Markdown from ${apiURL}`
+					);
 				}
-			}
+			});
+			response.on("error", () => {
+				console.error(`[Smort] Error fetching Markdown ${apiURL}`);
+			});
+			response.on("data", (chunk: any) => {
+				body += chunk;
+			});
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		request.end();
 	}
 
-	onunload() {
+	async addMarkdown(md: string) {
+		const activeView = this.getActiveView();
+		if (!activeView) {
+			console.error("[Smort] No active view to insert into.");
+			return;
+		}
 
+		activeView.editor.replaceSelection(md);
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+	getActiveView(): MarkdownView {
+		return this.app.workspace.getActiveViewOfType(MarkdownView);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class URLModal extends Modal {
+	plugin: Smort;
+
+	constructor(app: App, plugin: Smort) {
 		super(app);
+		this.plugin = plugin;
+		this.modalEl.id = "smort-modal";
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		addIcon("smort-logo", smortLogo);
+		contentEl.createEl("h1", {
+			text: "Smort.io",
+			cls: "smort-title",
+		});
+		const urlField = new TextComponent(contentEl).setPlaceholder(
+			"https://smort.io/[...]"
+		);
+		urlField.inputEl.id = "smort-input";
+
+		const getSmort = () => {
+			const url = urlField.getValue();
+			this.plugin.processURL(url);
+			this.close();
+		};
+
+		const smortButton = new ButtonComponent(contentEl)
+			.setButtonText("Get Markdown")
+			.onClick(getSmort);
+		smortButton.buttonEl.id = "smort-button";
+		urlField.inputEl.focus();
+		urlField.inputEl.addEventListener("keypress", function (keypressed) {
+			if (keypressed.key === "Enter") {
+				getSmort();
+			}
+		});
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
